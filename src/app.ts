7428eda203872b834af4b604a9c91ceb6cde567e
Ankit Sharma <me@ankit.party>
2019-03-26 07:27:48 -0700

    I decided I liked semicolons

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,42 @@
 'use strict'
-const axios = require('axios')
+const axios = require('axios');
 
 function main () {
   if (process.argv.length < 3) {
-    console.log('No location provided.')
-    return
+    console.log('No location provided.');
+    return;
   }
 
-  var locate = 'https://www.mapquestapi.com/geocoding/v1/address?key=YOUR_KEY&location='
+  var locate = 'https://www.mapquestapi.com/geocoding/v1/address?key=YOUR_KEY&location=';
 
   // Appends args with spaces into the URL
   for (let j = 2; j < process.argv.length; j++) {
-    locate += process.argv[j]
+    locate += process.argv[j];
     if (process.argv[j + 1]) {
-      locate += ' '
+      locate += ' ';
     }
   }
-  geocode(locate)
+  geocode(locate);
 }
 
 // Converts address/postcode to GPS coordinates
 function geocode (locate) {
   axios.get(locate)
     .then(function (response) {
-      var lat = response.data.results[0].locations[0].latLng.lat
-      var long = response.data.results[0].locations[0].latLng.lng
-      console.log('Lat: ' + lat + ' Long: ' + long)
-      aqius(lat, long)
+      var lat = response.data.results[0].locations[0].latLng.lat;
+      var long = response.data.results[0].locations[0].latLng.lng;
+      console.log('Lat: ' + lat + ' Long: ' + long);
+      aqius(lat, long);
     })
 }
 
 // Retrieves AQIUS from GPS coordinates
 function aqius (lat, long) {
-  var geocode = 'https://api.airvisual.com/v2/nearest_city?lat=' + lat + '&lon=' + long + '&key=YOUR_KEY'
+  var geocode = 'https://api.airvisual.com/v2/nearest_city?lat=' + lat + '&lon=' + long + '&key=YOUR_KEY';
   axios.get(geocode)
     .then(function (response) {
-      console.log('Air quality index (AQIUS): ' + response.data.data.current.pollution.aqius)
+      console.log('Air quality index (AQIUS): ' + response.data.data.current.pollution.aqius);
     })
 }
 
-main()
+main();
